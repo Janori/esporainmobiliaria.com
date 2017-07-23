4b515b42ffdff2853528595ffa22d5ca671d5474
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2017-07-23 00:17:35 -0500

    Corrección de modelo User.

--- src/app/shared/model/User.ts
+++ src/app/shared/model/User.ts
@@ -1,22 +1,36 @@
 export class User {
 	public id: Number;
-	public status: string;
 	public name: string;
-	public first_surname: string;
-	public second_surname: string;
+	public username: string;
 	public email: string;
 	public password : string;
+	public remember_token: string;
+	public img_path: string;
+	public first_surname: string;
+	public second_surname: string;
+	public gender: string;
+	public mariage_status: string;
+	public address: string;
+	public kind: string;
 	public created_at: string;
 	public updated_at: string;
 
+	public static NO_USER_PATH = '/images/default-user-image.png';
+
 	constructor(obj?: any) {
 		this.id = obj && obj.id || 0;
-		this.status = obj && obj.status || 'Activo';
 		this.name = obj && obj.name || '';
-		this.first_surname = obj && obj.first_surname || '';
-		this.second_surname = obj && obj.second_surname || '';
+		this.username = obj && obj.username || '';
 		this.email = obj && obj.email || '';
 		this.password  = obj && obj.password || '';
+		this.remember_token = null;
+		this.img_path = obj && obj.img_path || User.NO_USER_PATH;
+		this.first_surname = obj && obj.first_surname || '';
+		this.second_surname = obj && obj.second_surname || '';
+		this.gender = obj && obj.gender || '';
+		this.mariage_status = obj && obj.mariage_status || '';
+		this.address = obj && obj.address || '';
+		this.kind = obj && obj.kind || '';
 		this.created_at = obj && obj.created_at || null;
 		this.updated_at = obj && obj.updated_at || null;
 	}
