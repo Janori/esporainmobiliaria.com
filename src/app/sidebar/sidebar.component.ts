89cd930a28035bb0e6218267a52a7cdc5b6ea98d
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2017-07-22 22:08:04 -0500

    Conexión del sidebar.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -16,7 +16,7 @@ export class SidebarComponent implements OnInit {
     ngOnInit() {
         this._userService.getMenu().subscribe(
             result => {
-                console.log(result);
+                this.options = result.data;
             },
             error => {
                 console.log(error);
@@ -24,18 +24,4 @@ export class SidebarComponent implements OnInit {
             }
         );
     }
-    //
-    // setOptionsByType = (type) => {
-    //
-    //     // TODO logica para el cambio de opciones según el rol.
-    //
-    //     this.options.push({title: 'Inicio', icon: 'icon-rocket', url: ''});
-    //     this.options.push({title: 'Agentes', icon: 'icon-users', url: 'agentes'});
-    //     this.options.push({title: 'Sucursales', icon: 'icon-briefcase', url: 'sucursales'});
-    //     this.options.push({title: 'Inmuebles', icon: 'icon-home', url: 'inmuebles'});
-    //     this.options.push({title: 'Prospectos', icon: 'icon-user-follow', url: 'prospectos'});
-    //     this.options.push({title: 'Ventas', icon: 'icon-bar-chart', url: 'ventas'});
-    //     this.options.push({title: 'Geolocalización', icon: 'icon-pointer', url: 'geolocalización'});
-    // }
-
 }
