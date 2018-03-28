ad52ffb53faf22b1ca63698202c940c64a203ac3
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2018-03-28 00:44:58 -0600

    Configuración para api.

--- src/app/shared/services/Service.ts
+++ src/app/shared/services/Service.ts
@@ -6,9 +6,9 @@ export class Service {
     public url: string;
 
     constructor() {
-        // if(!/localhost/.test(document.location.host))
-            // this.url = 'https://www.esporainmobiliaria.com/api/'; // prod
-        // else
+        if(!/localhost/.test(document.location.host))
+            this.url = 'https://www.esporainmobiliaria.com/api/'; // prod
+        else
             this.url = 'http://esporainmobiliaria.com/api/'; // test
     }
 
